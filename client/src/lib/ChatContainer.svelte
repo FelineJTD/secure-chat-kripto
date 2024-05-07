8b71a691705eb2bb8808058ae80c35fe6332de95
FelineJTD <13520050@std.stei.itb.ac.id>
2024-05-08 01:14:52 +0700

    feat: chat input

--- client/src/lib/ChatContainer.svelte
+++ client/src/lib/ChatContainer.svelte
@@ -1,3 +1,3 @@
-<div class="max-w-[48rem] min-h-screen m-auto flex flex-col p-12 gap-4 bg-[url('../src/assets/patterns/topography.svg')] bg-repeat">
+<div class="max-w-[48rem] min-h-screen m-auto flex flex-col px-8 py-4 gap-4 bg-[url('../src/assets/patterns/topography.svg')] bg-repeat relative">
   <slot />
 </div>
---
+++ client/src/lib/Container.svelte
@@ -0,0 +1,3 @@
+<div class="max-w-[48rem] min-h-screen m-auto flex flex-col bg-[url('../src/assets/patterns/topography.svg')] bg-repeat relative">
+  <slot />
+</div>
