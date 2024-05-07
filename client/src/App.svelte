22842b54a722f53ace28be5e4f4eb8373f7fcf68
FelineJTD <13520050@std.stei.itb.ac.id>
2024-05-08 00:54:42 +0700

    feat: bg

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import ChatBubble from "./lib/ChatBubble.svelte";
+  import ChatContainer from "./lib/ChatContainer.svelte";
 </script>
 
 <main class="bg-neutral-100">
-  <div class="max-w-[48rem] min-h-screen m-auto flex flex-col p-12 gap-4">
+  <ChatContainer>
     <ChatBubble message={`Hello, \nWorld!`} />
     <ChatBubble isSelf message="Hello, World!" />
     <ChatBubble isSelf message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-  </div>
+  </ChatContainer>
 </main>
 
 <style>
---
+++ client/src/lib/ChatContainer.svelte
@@ -0,0 +1,3 @@
+<div class="max-w-[48rem] min-h-screen m-auto flex flex-col p-12 gap-4 bg-[url('../src/assets/patterns/topography.svg')] bg-repeat">
+  <slot />
+</div>
