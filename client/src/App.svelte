a7dd937b789a3fd82b9979c9251c9707c30b3b22
FelineJTD <13520050@std.stei.itb.ac.id>
2024-05-09 17:32:01 +0700

    feat: initiate websocket

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -4,6 +4,24 @@
   import ChatHeader from "./lib/ChatHeader.svelte";
   import ChatInput from "./lib/ChatInput.svelte";
   import Container from "./lib/Container.svelte";
+  import { onMount } from "svelte";
+
+  let socket: WebSocket
+  onMount(() => {
+    socket = new WebSocket("ws://localhost:8080/chat")
+    socket.addEventListener("open", ()=> {
+      console.log("Opened")
+    })
+
+    socket.addEventListener("message", (event) => {
+      console.log("Message from server ", event.data)
+    })
+  })
+
+  const onSend = (message: string) => {
+    socket.send(message)
+  }
+  
 </script>
 
 <main class="bg-neutral-100">
@@ -19,7 +37,7 @@
         <ChatBubble isSelf message="Hello, World!" />
         <ChatBubble isSelf message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
       </ChatContainer>
-      <ChatInput />
+      <ChatInput onSend={onSend} />
     </Container>
   </div>
 </main>
--- client/src/lib/ChatInput.svelte
+++ client/src/lib/ChatInput.svelte
@@ -1,7 +1,20 @@
-<div class="flex sticky bottom-0 px-4 md:px-8 py-4 bg-neutral-200">
-  <input type="text" class="w-full border border-neutral-400 rounded-xl p-2 mr-4" />
+<script lang="ts">
+  export let onSend: (message: string) => void
+
+  let input: HTMLInputElement
+
+  const sendMessage = (e: Event) => {
+    const formData = new FormData(e.target as HTMLFormElement)
+    const message = formData.get("message") as string
+    onSend(message)
+    input.value = ""
+  }
+</script>
+
+<form class="flex sticky bottom-0 px-4 md:px-8 py-4 bg-neutral-200" on:submit|preventDefault={sendMessage}>
+  <input bind:this={input} name="message" type="text" class="w-full border border-neutral-400 rounded-xl p-2 mr-4" />
   <button class="bg-neutral-600 text-neutral-100 rounded-xl py-2 px-4 flex items-center hover:bg-neutral-700 duration-100">
     <p class="mr-2 mb-0.5">Send</p>
     <img src="../src/assets/icons/paper-plane-right.svg" alt="" class="w-4 h-4 mr-2" />
   </button>
-</div>
+</form>
